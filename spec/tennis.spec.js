0f55b11f1dabbb9996a741c33cb1c59ed79527cd
Jeerapat Sritawan <talesrunner_dear123@hotmail.com>
2016-09-27 15:28:13 +0700

    add function start game

--- spec/tennis.spec.js
+++ spec/tennis.spec.js
@@ -1,3 +1,24 @@
+function Tennis(){
+  var scoreA = 0;
+  var scoreB = 0;
+
+  function playerAGetScore(){
+
+  }
+  function playerBGetScore(){
+
+  }
+  this.start = function(){
+    scoreA = 0;
+    scoreB = 0;
+  }
+  this.echo = function(){
+    if(scoreA == 0 && scoreB == 0){return 'Love - Love';}
+  }
+
+
+}
+
 describe("Tennis game", function() {
   it('should echo "Love - Love" when starting the game', function() {
     var tennis = new Tennis();

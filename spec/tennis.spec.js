a6d3f049314a1ebbd4bd810ed405b29c548fe99c
Jeerapat Sritawan <talesrunner_dear123@hotmail.com>
2016-09-27 16:12:49 +0700

    add echo love-thirty

--- spec/tennis.spec.js
+++ spec/tennis.spec.js
@@ -34,6 +34,7 @@ function Tennis(){
     else if (scoreA == 30 && scoreB == 0){return 'Thirty - Love';}
     else if (scoreA == 40 && scoreB == 0){return 'Forty - Love';}
     else if (scoreA == 0 && scoreB == 15){return 'Love - Fifteen';}
+    else if (scoreA == 0 && scoreB == 30){return 'Love - Thirty';}
   }
 
 

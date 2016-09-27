3b425b36ad30f3c970b5508b91069ee1602b7687
Jeerapat Sritawan <talesrunner_dear123@hotmail.com>
2016-09-27 15:46:33 +0700

    add Test Fifteen-love

--- spec/tennis.spec.js
+++ spec/tennis.spec.js
@@ -2,11 +2,27 @@ function Tennis(){
   var scoreA = 0;
   var scoreB = 0;
 
-  function playerAGetScore(){
-
+  this.playerAGetScore = function(){
+    if(scoreA == 0){
+      scoreA += 15;
+    }else if(scoreA == 15){
+      scoreA += 15;
+    }else if(scoreA == 30){
+      scoreA += 10;
+    }else if(scoreA == 40){
+      scoreA += 10;
+    }
   }
-  function playerBGetScore(){
-
+  this.playerBGetScore = function(){
+    if(scoreB == 0){
+      scoreB += 15;
+    }else if(scoreB == 15){
+      scoreB += 15;
+    }else if(scoreB == 30){
+      scoreB += 10;
+    }else if(scoreB == 40){
+      scoreB += 10;
+    }
   }
   this.start = function(){
     scoreA = 0;
@@ -24,5 +40,10 @@ describe("Tennis game", function() {
     var tennis = new Tennis();
     tennis.start();
     expect(tennis.echo()).toEqual('Love - Love');
+  });
+  it('should echo "Fifteen - Love" when player A get first score', function() {
+    var tennis = new Tennis();
+    tennis.playerAGetScore();
+    expect(tennis.echo()).toEqual('Fifteen - Love');
   });
 });
